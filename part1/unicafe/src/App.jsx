2c8b452c50ec27ecec50a0738ee93ad6b788af54
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-11-14 17:46:24 +0700

    add components that handle statistic and button

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+
+const Statistics = ({ good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  const average = (good * 1) + (neutral * 0) + (bad * -1)/ total;
+  const positiveReviews = (good / total) * 100;
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral {neutral}:</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average:{average}</p>
+      <p>Positive:{positiveReviews}</p>
+    </div>
+  )
+};
+
+const Button = ({ initial, setInitial, text }) => {
+  const handleClick = () => {
+    setInitial(initial + 1)
+  };
+
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+};
 
 
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+
   return (
-    <h1>Hello world</h1>
+    <div>
+      <h1>Give Feedback</h1>
+      <Button initial={good} setInitial={setGood} text='Good' />
+      <Button initial={neutral} setInitial={setNeutral} text='Neutral' />
+      <Button initial={bad} setInitial={setBad} text='Bad' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
