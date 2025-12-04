28c683ca0b82add9a9785d85cd2c557ca33376a4
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-12-04 14:27:20 +0700

    create middleware file
    middleware handles reqLogger, unknown endpoint and errorhandler

--- part4/blog/backend/index.js
+++ part4/blog/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
+const middleware = require('./utils/middleware');
 
 const app = express();
 
@@ -14,11 +15,18 @@ mongoose
     })
     .catch(error => {
         logger.error('Error connection to MongoDB', error.message);
-    })
+    });
+
+app.use(express.json());
+app.use(middleware.reqLogger);
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>')
 });
+
+// 404 & error
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 app.listen(config.PORT, () => {
     console.log(`Server is running on http://localhost:${config.PORT}`);
---
+++ part4/blog/backend/utils/middleware.js
@@ -0,0 +1,28 @@
+const logger = require('./logger');
+
+const reqLogger = (req, res, next) => {
+    logger.info('Method', req.method);
+    logger.info('Path', req.path);
+    logger.info('Body', req.body);
+    logger.info('---');
+}
+
+// 404 endpoint
+const unknownEndpoint = (req, res) => {
+    res.staus(404).send({ error: 'Unknown endpoint '});
+}
+
+// error handler
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message);
+
+    if (err.name === 'CastError') return res.status(400).send({ error: 'Malformed id' });
+    if (err.name === 'ValidationError') return res.status(400).json({ error: err.message });
+    next(err);
+};
+
+module.exports = {
+    reqLogger,
+    unknownEndpoint,
+    errorHandler
+}
